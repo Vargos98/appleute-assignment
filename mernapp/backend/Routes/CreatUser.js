9bfd91e2fe2fc597c2acbfb8ce48ee6a456a2a02
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2023-12-01 23:18:54 +0530

    Added JWT authentication and bcrypt hashing with added secret msg also checked it on the console

--- mernapp/backend/Routes/CreatUser.js
+++ mernapp/backend/Routes/CreatUser.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User')
-
-
-
+const User = require('../models/User');
+const jwt = require("jsonwebtoken");
+const bcrypt =  require("bcryptjs");
+const jwtSecret = "MynaMeIsUmeshKumarandImTryingToBeaWebDeveloper";
 const { body, validationResult } = require('express-validator');
 
 // now we check the req sent from the body full-fills our requirements <just like how we add "required in the entry field of passwords and emails";
@@ -14,15 +14,20 @@ router.post("/creatuser", [
 
     , async (req, res) => {
 
+
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
 
+        const salt = await bcrypt.genSalt(10);
+        let secPassword = await bcrypt.hash(req.body.password,salt);
+
+
         try {
             await User.create({
                 name: req.body.name,
-                password: req.body.password,
+                password: secPassword,
                 email: req.body.email,
                 location: req.body.location
             })
@@ -53,14 +58,18 @@ router.post("/loginuser", [
             if (!userData) {
                 return res.status(400).json({ errors: "try logging in with correct email" });
             }
-
-            if (req.body.password !== userData.password) {
+            const pwdCompare = await bcrypt.compare(req.body.password, userData.password)
+            if (!pwdCompare) {
                 return res.status(400).json({ errors: "try logging in with correct password" });
             }
-            return res.json({ success: true });
-
-
-
+            const data = {
+                user:{
+                    id:userData.id
+                }
+            }
+            const authToken =jwt.sign(data, jwtSecret)
+            return res.json({ success: true, authToken:authToken });
+           
         } catch (error) {
             console.log(error);
             res.json({ success: false });
