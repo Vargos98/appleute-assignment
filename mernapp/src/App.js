187b55bcee058ee761c7416cbee90a63a32d8391
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2023-12-01 14:42:04 +0530

    Created Card component, carousel, footer and Navbar
    designed these using Bootstap-5 will add functionality later

--- mernapp/src/App.js
+++ mernapp/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import Home from './screens/Home'
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Login from './screens/Login';
 
 export default function App() {
   return (
-    <div>
-      <span>
-        <Home />
-      </span>
-
-    </div>
+    <Router>
+      <div>
+        <Routes>
+            <Route exact path='/' element={<Home/>} />
+            <Route exact path='/login' element={<Login/>} />
+        </Routes>
+      </div>
+    </Router>
 
   )
 }
