e90ba50e91166b1a1f9adf7351c169f695f0909a
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2023-12-01 16:00:49 +0530

    Update Navbar.jsx

--- mernapp/src/components/Navbar.jsx
+++ mernapp/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from './Logo2.png'
 
 export default function Navbar() {
