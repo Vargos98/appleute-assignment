1bda4d6eda231730d59ad6769350fad07ef6b7cc
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2023-12-02 09:50:38 +0530

    Updated Index.js, Db.js and DisplayData.js
    Also fetched data from foodCategory.json,
    Created routes to Via DisplayData.js

---
+++ mernapp/backend/Routes/DisplayData.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/foodData',(req,res)=>{
+    try {
+        res.send([global.food_items, global.foodCategory]);
+        console.log(global.food_items)
+        
+    } catch (error) {
+
+        console.log(error.message);
+        res.send("Server Crashed");
+        
+    }
+})
+
+module.exports =router;
--- mernapp/backend/db.js
+++ mernapp/backend/db.js
@@ -1,30 +1,48 @@
 const mongoose = require('mongoose');
 //               mongodb+srv://vargos98:<password>@cluster0.j8cqdbr.mongodb.net/<databaseName>?retryWrites=true&w=majority
-const mongoURI= 'mongodb+srv://vargos98:9828206Uk@cluster0.j8cqdbr.mongodb.net/gofoodmern?retryWrites=true&w=majority'
+const mongoURI = 'mongodb+srv://vargos98:9828206Uk@cluster0.j8cqdbr.mongodb.net/gofoodmern?retryWrites=true&w=majority'
 
-const mongoDB = async()=>{
+const mongoDB = async () => {
 
     //after we have defined the endpoint in the mongURI : we need to connect it.
-    await mongoose.connect(mongoURI, {useNewUrlParser:true}, (err,result)=>{
-        if(err) 
-        {
+    await mongoose.connect(mongoURI, { useNewUrlParser: true }, (err, result) => {
+        if (err) {
             console.log("---", err);
-        }else{
+        } else {
             // after this we need to fetch the data from the collection -> database named: food_items. 
             console.log("Connected successfully");
-            const fetched_data=  mongoose.connection.db.collection("food_items");
-            fetched_data.find({}).toArray(function(err,data){
-                if(err){
-                    console.log(err);
+            const fetched_data = mongoose.connection.db.collection("food_items");
+            fetched_data.find({}).toArray(async function (err, data) {
 
-                }else{
-                    // to check the data is recieved or not ?
+                const foodCategory = await mongoose.connection.db.collection("foodCategory");
+                foodCategory.find({}).toArray(function (err, catData) {
 
-                    // console.log(data);
-                }
+                    if (err) {
+                        console.log(err);
+
+                    } else {
+                        // to check the data is recieved or not ?
+                        // console.log(data);
+
+                        global.food_items = data;
+                        global.foodCategory = catData;
+                        // console.log(global.food_items);
+                    }
+
+                })
+                // if(err){
+                //     console.log(err);
+
+                // }else{
+                //     // to check the data is recieved or not ?
+                //     // console.log(data);
+
+                //     global.food_items =data;
+                //     // console.log(global.food_items);
+                // }
             });
-    }
-        
+        }
+
     });
 }
 
